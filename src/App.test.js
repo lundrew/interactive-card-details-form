27892e7460bbf9ef0f0341134a7d6f7aea2acbf5
lundrew <92401835+lundrew@users.noreply.github.com>
2022-12-02 13:10:31 -0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Hello", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("App renders without errors", () => {
+  const app = render(<App />);
+  expect(app).not.toThrowError;
 });
